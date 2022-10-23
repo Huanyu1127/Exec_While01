000d5a3eb72b78f1864a4999e862bb92f04ad910
User <User@LAPTOP-SPFF78FA>
2022-10-23 16:09:15 +0800

    加入專案檔案。

---
+++ Q1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+	//1+ 2 + 3 + 4 + .... + N <105請問 N 是多少?
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int sum = 0;
+			int first = 1;
+			while (sum < 105)
+			{
+				sum += first;
+				first++;
+			}
+			Console.WriteLine(first - 2); //N是13 N是14剛好=105
+
+			//int sum = 0;
+			//int first = 0;
+			//while (sum < 105)
+			//{
+			//	first++;
+			//	sum += first;
+			//}
+			//Console.WriteLine(first - 1); //N是13
+		}
+	}
+}
---
+++ Q2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+	//使用 while , 在畫面顯示所有可以整除 300 的整數，例如: 1, 2, 3, 4, 5, 6, 10, ...., 300
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int number = 1;
+			while (number <= 300)
+			{
+				if (300 % number == 0)
+				{
+					Console.WriteLine(number);
+				}
+				number++;
+			}
+		}
+	}
+}
